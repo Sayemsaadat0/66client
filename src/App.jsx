30a923f1923e39a2c50474b16828acc2750ae48b
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-06 12:37:03 +0600

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [user , setUser] = useState('')
+  useEffect(
+    ()=>{
+      fetch('http://localhost:7979/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+    },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="text-white text-center bg-[url('https://img.freepik.com/free-vector/gradient-hexagonal-background_23-2148944164.jpg?w=826&t=st=1683353679~exp=1683354279~hmac=8903a97e334886df4fa95d05ef44e5d52b3cd0e651d636e49df72a7bd6854472')]">
+      <h1 className=''> 66 client</h1>
 
-export default App
+      <div class="flex justify-center items-center h-screen text-center text-white">
+        <form class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 hover:shadow-green-500">
+          <div class="mb-4">
+            <label class="block  font-bold mb-2" for="email">
+              Email
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="email" type="email"  required/>
+          </div>
+          <div class="mb-6">
+            <label class="block  font-bold mb-2" for="password">
+              Password
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"  />
+          </div>
+          <div class="flex items-center justify-center">
+            <button class="btn hover:bg-green-500" type="button">
+              Sign In
+            </button>
+          </div>
+          <div>
+        {
+          <h2 className='text-yellow-300'>
+           there are  {user.length} users
+          </h2>
+        }
+      </div>
+        </form>
+      </div>
+      
+
+    </div>
+  );
+};
+
+export default App;
